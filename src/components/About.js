51d08c90ff2c9118cc42e09968cbbc0f59c95757
Megha Maitin <megha.maitin2020@vitstudent.ac.in>
2022-02-08 23:50:14 +0530

    first

--- src/components/About.js
+++ src/components/About.js
@@ -1,19 +1,23 @@
 import React from 'react'
 import rect from "../images/Rectangle11.svg"
+import arrow from "../images/ExpandArrow.svg"
 const About = () => {
     return (
         <div>
             <div className="Container">
                 <div className="row">
-                    <div className = "col-lg-7 col-md-7">
-                        <h2>About Us</h2>
-                        <p>We are a team of 4 students from VIT Vellore aiming to find a solution to make diagnosis cheaper and faster.</p>
+                    <div className="col-lg-7 col-md-7">
+                        <p className="about">About Us</p>
+                        <p className="acon">We are a team of 4 students from VIT Vellore aiming to find a solution to make diagnosis cheaper and faster.</p>
                         <div class="card1">
-                            <p>Megha Maitin</p>
-                            <p>2nd Year BTech,VIT</p>
+                            <div>
+                                <p>Megha Maitin</p>
+                                <p className="fixed">2nd Year BTech,VIT</p>
+                            </div>
+                            <img id="arrow" src={arrow} />
                         </div>
                     </div>
-                    <div className ="col-lg-5 col-md-5">
+                    <div className="col-lg-5 col-md-5">
                         <img id="rect" src={rect} />
                     </div>
                 </div>
--- src/components/Mainsection.js
+++ src/components/Mainsection.js
@@ -4,6 +4,7 @@ import tbox from "../images/Group18.svg"
 import bbox from "../images/Group19.svg"
 const Mainsection = () => {
     return (
+        <mainsection>
         <div className="container-fluid">
             <div className="row">
                 <div class="col-lg-5 col-md-12">
@@ -12,30 +13,23 @@ const Mainsection = () => {
                     <form>
                         <input type="text" value="I am a doctor" />
                     </form>
-                    <button className="mainbtn">SIGN IN</button>
-                    <button className="mainbtn">REGISTER</button>
+                    <div className="btnsec">
+                        <button className="mainbtn">SIGN IN</button>
+                        <div className="space"></div>
+                        <button className="mainbtn">REGISTER</button>
+                    </div>
                 </div>
                 <div className="col-lg-3 ">
                     <img id="bbox" src={bbox} />
                     <img id="lbox" src={lbox} />
-                    </div>
-                    <div className="col-lg-4 ">
+                </div>
+                <div className="col-lg-4 ">
                     <img id="tbox" src={tbox} />
                     <img id="doc" src={girl} />
                 </div>
             </div>
-            {/* <p className="firsttext">Get Your Diagnosis</p>
-            <p className="Secondtext">Within Seconds</p>
-            <form>
-                <input type="text" value="doctor" />
-            </form>
-            <button>SIGN IN</button>
-            <button>REGISTER</button>
-            <div className="right">
-                <img id="lbox" src={lbox} />
-                <img id="doc" src={girl} />
-            </div> */}
         </div>
+        </mainsection>
     )
 }
 
